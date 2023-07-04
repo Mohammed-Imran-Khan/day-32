f731f83419a7ac21d5bc275a04e48472e22bad63
Mohammed-Imran-Khan <127581145+Mohammed-Imran-Khan@users.noreply.github.com>
2023-07-04 13:15:45 +0530

    Add files via upload

---
+++ src/Data/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: "34567896",
+      name: "1984",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51EJKs3rPwL._SX316_BO1,204,203,200_.jpg",
+      author: "George Orwell",
+      description: "A dystopian novel that explores themes of totalitarianism, surveillance, and the power of language."
+    },
+    {
+      id: "345678907",
+      name: "Pride and Prejudice",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51YSHZkntuL._SX326_BO1,204,203,200_.jpg",
+      author: "Jane Austen",
+      description: "A classic romance novel featuring witty social commentary and memorable characters."
+    },
+    {
+      id: "3456788",
+      name: "The Catcher in the Rye",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51k+01LQmZL._SX324_BO1,204,203,200_.jpg",
+      author: "J.D. Salinger",
+      description: "A coming-of-age novel that explores teenage rebellion, identity, and the challenges of growing up."
+    },
+    {
+      id: "3456789",
+      name: "The Lord of the Rings",
+      image: "https://images-na.ssl-images-amazon.com/images/I/91uJ6PpEYcL.jpg",
+      author: "J.R.R. Tolkien",
+      description: "An epic fantasy trilogy that follows a quest to destroy a powerful ring and save Middle-earth."
+    },
+    {
+      id: "145674567",
+      name: "Harry Potter and the Philosopher's Stone",
+      image: "https://images-na.ssl-images-amazon.com/images/I/51CJMC8VzzL._SX330_BO1,204,203,200_.jpg",
+      author: "J.K. Rowling",
+      description: "The first book in the beloved Harry Potter series, introducing readers to the magical world of Hogwarts."
+    }
+  ];
+  
